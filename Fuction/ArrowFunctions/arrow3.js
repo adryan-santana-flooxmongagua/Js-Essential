9f9da636024d69a79656bd2a8aba5b5cbd26a49d
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-04-15 10:54:09 -0300

    feat: implement third part of arrow functions

---
+++ Fuction/ArrowFunctions/arrow3.js
@@ -0,0 +1,18 @@
+let comparacomThis = function(param){
+    console.log(this ===param)
+}
+
+comparacomThis(global)
+
+const obj = {};
+comparaComThis = comparaComThis.bind(obj);
+comparaComThis(global);
+comparaComThis(obj);
+
+let comparaComThisArrow = param => console.log(this === param);
+comparaComThisArrow(global);
+comparaComThisArrow(module.exports);
+
+comparaComThisArrow = comparaComThisArrow.bind(obj);
+comparaComThisArrow(obj);
+comparaComThisArrow(module.exports);
